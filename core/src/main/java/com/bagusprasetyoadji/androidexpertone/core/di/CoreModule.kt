070de8885ae7a68e737ef0f3a2bf8c9482829971
Bagus Prasetyo Adji <bagusprasetyaadji@gmail.com>
2021-05-25 19:36:32 +0700

    Final Submission MADE 2

--- core/src/main/java/com/bagusprasetyoadji/androidexpertone/core/di/CoreModule.kt
+++ core/src/main/java/com/bagusprasetyoadji/androidexpertone/core/di/CoreModule.kt
@@ -8,6 +8,7 @@ import com.bagusprasetyoadji.androidexpertone.core.data.source.remote.RemoteData
 import com.bagusprasetyoadji.androidexpertone.core.data.source.remote.network.ApiService
 import com.bagusprasetyoadji.androidexpertone.core.domain.repository.IMovieRepository
 import com.bagusprasetyoadji.androidexpertone.core.utils.AppExecutors
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -28,10 +29,15 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
