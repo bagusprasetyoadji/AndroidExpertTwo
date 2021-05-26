e0f0e23f463d64035fa7e2bb1030132f8b54dca4
Bagus Prasetyo Adji <bagusprasetyaadji@gmail.com>
2021-05-26 08:49:15 +0700

    Final Submission MADE 2

--- favorite/src/main/java/com/bagusprasetyoadji/androidexpertone/favorite/FavoriteViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.bagusprasetyoadji.androidexpertone.favorite
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import com.bagusprasetyoadji.androidexpertone.core.domain.usecase.MovieUseCase
-
-class FavoriteViewModel(movieUseCase: MovieUseCase) : ViewModel() {
-    val favoritemovie = movieUseCase.getFavoriteMovie().asLiveData()
-}
